070a3f04e34c4ec6ad7b532290495ca88a640a2e
Tabetha Boushey <tabetharaeboushey@gmail.com>
2014-09-24 14:44:16 -0600

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,13 @@
+/**
+ * @author Tabetha Boushey
+ * Date: 6/25/2013
+ * CSCI232 Lab 1
+ * Class: Driver
+ */
+
+public class Driver {
+
+    public static void main(String[] args) {
+        TreeFrame.getInstance();
+    }
+}
